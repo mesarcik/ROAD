9fc9f5461bbae02363cb2475a021b59e50a5358e
Misha Mesarcik <m.mesarcik@uva.nl>
2022-09-22 13:14:58 +0200

    patch creation working

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 import cv2
 from tqdm import tqdm
 import copy
@@ -21,11 +22,14 @@ def remove_singles(data: list, *args) -> list:
     _args = []
     indx = [i for i, d in enumerate(data) if d.shape[1] !=1]
     _data = [_data[i] for i in indx]
-    for a in args:
-        for i in indx:
-            _args.append(a[i])
 
-    return _data,_args
+    if args:
+        for a in args:
+            for i in indx:
+                _args.append(a[i])
+
+        return _data,_args
+    return _data
 
 def reshape(data: list, dim: list, verbose:bool=False) -> np.array:
     """
@@ -50,3 +54,52 @@ def reshape(data: list, dim: list, verbose:bool=False) -> np.array:
 
     return np.array(_data)
         
+def patch(data: torch.Tensor, patch_size:int, verbose:bool=False) -> torch.Tensor:
+    """
+        Makes (N,C,h,w) shaped tensor into (N*(h/size)*(w/size),C,h/size, w/size)
+        Note: this only works for square patches sizes
+        
+        Parameters
+        ----------
+        data: Tensor in form (N,C,h,w)
+        patch_size: square patch size
+        verbose: prints tqdm output
+
+        Returns
+        -------
+        patches: Tensor of patches reshaped to (N*(h/size)*(w/size),C,h/size, w/size)
+
+    """
+    unfold = data.permute(0,2,3,1).unfold(1, patch_size, patch_size).unfold(2, patch_size, patch_size)
+    patches = unfold.contiguous().view(-1, data.shape[1], patch_size, patch_size)
+    return patches
+
+def unpatch(patches: torch.Tensor, patch_size:int, data_shape:tuple, verbose:bool=False) -> torch.Tensor:
+    """
+        Used to convert patches dataset into original dimenions
+        
+        Parameters
+        ----------
+        patches: Tensor in form (N,C,patch_size, patch_size)
+        patch_size: square patch size
+        data_shape: tuple of original data.shape
+        verbose: prints tqdm output
+
+        Returns
+        -------
+        data : Tensor of reconstructed patches
+
+    """
+    assert len(data_shape) == 4, "Data shape must be in form (N,C,...)"
+
+    n_patches = data_shape[-2]//patch_size # only for square patches 
+    N_orig = patches.shape[0]//n_patches**2
+    unfold_shape = (N_orig, n_patches, n_patches, data_shape[1], patch_size, patch_size)
+    
+    data = patches.view(unfold_shape)
+    data = data.permute(0, 3, 1, 4, 2, 5).contiguous()
+    data = data.view(data_shape)
+
+    return data
+
+
