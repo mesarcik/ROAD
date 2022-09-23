37e9d304138f9f38511bf6c0799b6dc7eac56493
Misha Mesarcik <m.mesarcik@uva.nl>
2022-09-23 16:09:20 +0200

    models training, but not producing correct outputs

--- data.py
+++ data.py
@@ -7,10 +7,11 @@ import _pickle as cPickle
 
 import torch 
 from torch.utils.data import Dataset, DataLoader
+from matplotlib import pyplot as plt
 
 
 class LOFARDataset(Dataset):
-    def __init__(self, dataset_dir, patch_size, sourceTransform):
+    def __init__(self, dataset_dir, patch_size, sourceTransform=None, limit =5000):# set default types
         self.dataset_dir = dataset_dir 
         self.patch_size = patch_size
 
@@ -21,8 +22,16 @@ class LOFARDataset(Dataset):
         # TODO: when reshaping we are destroying the frequency band information, when this is necessary I need to change the code
         self.data = self.reshape(self.data, (256,256))
 
-        self.data = np.clip(self.data, 0, np.percentile(self.data, 99))
-        self.data = (self.data - np.min(self.data)) / (np.max(self.data) - np.min(self.data))
+        ###########################################################################
+        mask = np.random.randint(low=0, high= 12285, size=limit) 
+        self.labels = np.array(self.labels, dtype='object')
+        self.data, self.labels = self.data[mask], self.labels[mask]
+        ###########################################################################
+
+        indxs = np.random.randint(low=0, high= len(self.data), size=16)
+        self.data = self.normalise(self.data)
+        #self.data = (self.data - np.min(self.data)) / (np.max(self.data) - np.min(self.data))
+        self.plot_spectra(self.data, indxs, '/tmp/sample')
         self.data = torch.from_numpy(self.data).permute(0,3,1,2)
         self.data = self.patch(self.data, self.patch_size)
 
@@ -37,14 +46,37 @@ class LOFARDataset(Dataset):
             idx = idx.tolist()
 
         datum = self.data[idx,...]
-        label = str('-'.join(self.labels[idx]))
+        label = ''#str('-'.join(self.labels[idx]))
 
         if self.sourceTransform:
             datum = self.sourceTransform(datum)
 
         return datum, label
 
+    def normalise(self, data:np.array)->np.array:
+        """
+            perpendicular polarisation normalisation
 
+        """
+        _data = np.zeros(data.shape)
+        for i, spec in enumerate(data):
+            for pol in range(4):
+                _min, _max = np.percentile(spec[...,pol], [5,95])
+                temp = np.clip(spec[...,pol],_min, _max)
+                temp  = (temp - np.min(temp)) / (np.max(temp) - np.min(temp))
+                _data[i,...,pol] = temp
+        _data = np.nan_to_num(_data, 0)
+        return _data
+
+    def plot_spectra(self, data: np.array, indxs: np.array, loc:str)->None:
+        sq = int(np.sqrt(len(indxs)))
+        fig, axs = plt.subplots(sq, sq, figsize=(10,10))
+        _c = 0
+        for i in range(sq):
+            for j in range(sq):
+                axs[i,j].imshow(data[indxs[_c],...,0], aspect='auto',interpolation='nearest', vmin =0, vmax=1)
+                _c+=1
+        plt.savefig(loc, dpi=300)
 
     def remove_singles(self, data: list, *args) -> list:
         """
@@ -64,13 +96,14 @@ class LOFARDataset(Dataset):
         _args = []
         indx = [i for i, d in enumerate(data) if d.shape[1] !=1]
         _data = [_data[i] for i in indx]
-
+        
         if args:
             for a in args:
+                temp_args = []
                 for i in indx:
-                    _args.append(a[i])
-
-            return _data,_args
+                    temp_args.append(a[i])
+                _args.append(temp_args)
+            return _data,*_args
         return _data
 
     def reshape(self, data: list, dim: list, verbose:bool=False) -> np.array:
@@ -87,49 +120,46 @@ class LOFARDataset(Dataset):
             _data: np.array of baselines with the same dimensions
 
         """
-        _data = []
-        for d in tqdm(data,disable=not verbose):
-            _temp = np.zeros((dim[0], dim[1],4))
+        _data = np.zeros((len(data), dim[0], dim[1], 4))
+        for i,d in enumerate(tqdm(data,disable=not verbose)):
             for p in range(4):
-                _temp[...,0] = cv2.resize(d[...,p], dim)
-            _data.append(_temp)
-
-        return np.array(_data)
+                _data[i,...,p] = cv2.resize(d[...,p], dim)
+        return _data
             
-    def patch(self, data: torch.Tensor, patch_size:int, verbose:bool=False) -> torch.Tensor:
+    def patch(self, data: torch.tensor, patch_size:int, verbose:bool=False) -> torch.tensor:
         """
             Makes (N,C,h,w) shaped tensor into (N*(h/size)*(w/size),C,h/size, w/size)
             Note: this only works for square patches sizes
             
             Parameters
             ----------
-            data: Tensor in form (N,C,h,w)
+            data: tensor in form (N,C,h,w)
             patch_size: square patch size
             verbose: prints tqdm output
 
             Returns
             -------
-            patches: Tensor of patches reshaped to (N*(h/size)*(w/size),C,h/size, w/size)
+            patches: tensor of patches reshaped to (N*(h/size)*(w/size),C,h/size, w/size)
 
         """
         unfold = data.permute(0,2,3,1).unfold(1, patch_size, patch_size).unfold(2, patch_size, patch_size)
         patches = unfold.contiguous().view(-1, data.shape[1], patch_size, patch_size)
         return patches
 
-    def unpatch(self, patches: torch.Tensor, patch_size:int, data_shape:tuple, verbose:bool=False) -> torch.Tensor:
+    def unpatch(self, patches: torch.tensor, patch_size:int, data_shape:tuple, verbose:bool=False) -> torch.tensor:
         """
             Used to convert patches dataset into original dimenions
             
             Parameters
             ----------
-            patches: Tensor in form (N,C,patch_size, patch_size)
+            patches: tensor in form (N,C,patch_size, patch_size)
             patch_size: square patch size
             data_shape: tuple of original data.shape
             verbose: prints tqdm output
 
             Returns
             -------
-            data : Tensor of reconstructed patches
+            data : tensor of reconstructed patches
 
         """
         assert len(data_shape) == 4, "Data shape must be in form (N,C,...)"
---
+++ main.py
@@ -0,0 +1,123 @@
+import torch 
+import torch.nn as nn
+from torchvision import transforms
+from torch.utils.data import DataLoader
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+import os
+
+from data import LOFARDataset
+from models import VAE
+from sklearn.manifold import TSNE
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+BATCHSIZE = 2**10
+PS=64
+LD=100
+
+# define dataset/dataloader
+#test_transform = transforms.Compose([transforms.ToTensor(),
+#                                     transforms.RandomCrop((128,128)),
+#                                 transforms.Normalize(DATA_MEANS, DATA_STD)
+#                                 ])
+
+# For training, we add some augmentation. Networks are too powerful and would overfit.
+#train_transform = transforms.Compose([transforms.RandomHorizontalFlip(),
+#                                      #transforms.RandomRotation([0, 180]),
+#                                      transforms.RandomCrop((128,128)),
+#                                      transforms.ToTensor(),
+#                                      transforms.Normalize(DATA_MEANS, DATA_STD)
+#                                     ])
+
+def imscatter(x, y, image, ax=None, zoom=1):
+    """
+        code adapted from: https://gist.github.com/feeblefruits/20e7f98a4c6a47075c8bfce7c06749c2
+    """
+    im = OffsetImage(image, zoom=zoom)
+    x, y = np.atleast_1d(x, y)
+    artists = []
+    for x0, y0 in zip(x, y):
+        ab = AnnotationBbox(im, (x0, y0), xycoords='data', frameon=False)
+        artists.append(ax.add_artist(ab))
+    ax.update_datalim(np.column_stack([x, y]))
+    ax.autoscale()
+    return artists
+
+# make dataset
+#TODO 
+train_dataset = LOFARDataset('/home/mmesarcik/data/LOFAR/compressed/organised/LOFAR_AD_dataset_21-09-22.pkl',
+                             PS,
+                             )
+#valid_dataset = HeraDataset('/home/mmesarcik/HERA_ResNet/HERA_val.pkl',test_transform) #test transforms are applied
+#test_dataset =  HeraDataset('/home/mmesarcik/HERA_ResNet/HERA_test.pkl',test_transform)
+
+train_dataloader = DataLoader(train_dataset, batch_size=BATCHSIZE, shuffle=True)
+#valid_dataloader = DataLoader(valid_dataset, batch_size=BS, shuffle=True)
+#test_dataloader = DataLoader(test_dataset, batch_size=BS, shuffle=False)
+
+# define model 
+vae = VAE(in_channels=4, latent_dim=LD,patch_size=PS, hidden_dims=[32,64,128,256]).to(device)
+vae.to(device)
+
+optimizer = torch.optim.Adam(vae.parameters(),lr=0.001)#, lr=0.0001, momentum=0.9)
+
+n_epochs = 100
+
+valid_loss_min = np.Inf
+train_loss = []
+train_acc = []
+total_step = len(train_dataloader)
+for epoch in range(1, n_epochs+1):
+    running_loss = 0.0
+    #print(f'Epoch {epoch}\n')
+    for batch_idx, (data_, target_) in enumerate(train_dataloader):
+        data_ = data_.float().to(device)
+        optimizer.zero_grad()
+
+        [_decoded, _input, _mu, _log_var] = vae(data_)
+        z = vae.reparameterize(_mu, _log_var)
+        loss = vae.loss_function(_decoded, _input, _mu, _log_var)
+        loss['loss'].backward()
+        optimizer.step()
+
+        running_loss += loss['loss'].item()
+        if (batch_idx) % 20 == 0:
+            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                   .format(epoch, n_epochs, batch_idx, total_step, loss['loss'].item()))
+    train_loss.append(running_loss/total_step)
+    print(f'\ntrain-loss: {np.mean(train_loss):.4f}')
+    batch_loss = 0
+
+    if True:
+        valid_loss_min = batch_loss
+        torch.save(vae.state_dict(), 'outputs/vae.pt')
+        print('Improvement-Detected, save-model')
+        vae.eval()
+        mu, log_var = vae.encode(data_)
+        Z = vae.reparameterize(mu, log_var).cpu().detach().numpy()
+        z = TSNE(n_components=2, learning_rate='auto',init='random', perplexity=3).fit_transform(Z)
+        
+        imgs = data_.cpu().detach().numpy()
+        xhats = _decoded.cpu().detach().numpy()
+        fig,axs = plt.subplots(10,2,figsize=(5,10));
+        for i in range(10):
+            r = np.random.randint(len(imgs))
+            axs[i,0].imshow(imgs[r,0,...], aspect='auto', interpolation='nearest')#, vmin=0, vmax=1)
+            axs[i,1].imshow(xhats[r,0,...], aspect='auto', interpolation='nearest')#, vmin=0, vmax=1)
+        plt.savefig('outputs/embeddings/epoch_output_{}'.format(epoch),dpi=98)
+        plt.close(fig)
+
+        fig,ax = plt.subplots(1,1,figsize=(10,10));
+        for x, y, image_path in zip(z[:,0], z[:,1], imgs[:,0,...]):
+            imscatter(x, y, image_path, zoom=0.7, ax=ax)
+        plt.savefig('outputs/embeddings/epoch_embedding_{}'.format(epoch),dpi=98)
+        plt.close('all')
+    vae.train()
+
+    plt.title("Train-Validation Accuracy")
+    plt.plot(train_loss, label='train')
+    plt.xlabel('num_epochs', fontsize=12)
+    plt.ylabel('accuracy', fontsize=12)
+    plt.savefig('/tmp/temp',dpi=300)
