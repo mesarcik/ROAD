c55626f5fe388642307e3b2593851316975d022f
mesarcik <m.mesarcik@uva.nl>
2022-10-10 16:37:45 +0200

    solar storm added

--- utils/data/defaults.py
+++ utils/data/defaults.py
@@ -1,1 +1,1 @@
-anomalies = ['oscillating_tile', 'electric_fence','data_loss', 'lightning','strong_radio_emitter']
+anomalies = ['oscillating_tile', 'electric_fence','data_loss', 'lightning','strong_radio_emitter', 'solar_storm']
